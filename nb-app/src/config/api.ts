752111967ec5140cbf7ec311978258c349db83b7
qqx <1542452647@qq.com>
2026-01-08 01:46:28 +0800

    fix: update API endpoint to banana2.peacedejiai.cc

--- nb-app/src/config/api.ts
+++ nb-app/src/config/api.ts
@@ -1,7 +1,6 @@
-export const DEFAULT_API_ENDPOINT = 'https://nanobanana2.peacedejiai.cc';
+export const DEFAULT_API_ENDPOINT = 'https://banana2.peacedejiai.cc';
 
 export const ALLOWED_ENDPOINT_HOSTS = [
-  'nanobanana2.peacedejiai.cc',
   'banana2.peacedejiai.cc',
   'peacedejiai.cc',
   'api.kuai.host',
